21bddc85002b75cf9a65b4387637bd3c69af275e
apodl <a.podlozny@gmail.com>
2023-07-28 23:03:33 +0300

    Integrated entsoe, started formatting prices

--- src/python/main.py
+++ src/python/main.py
@@ -1,19 +1,20 @@
 import tokens
+import price_processing
 
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sähkön hinnan kertova botti. Lähetä komento /hinta saadaksesi vastauksen.")
+    await context.bot.send_message(chat_id = update.effective_chat.id, text = "Sähkön hinnan kertova botti. Lähetä komento /hinta saadaksesi vastauksen.")
 
 async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="__Hintatiedot tähän__")
+    await context.bot.send_message(chat_id = update.effective_chat.id, text = price_processing.get_texts())
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(tokens.telegram_token).build()
--- src/python/price_processing.py
+++ src/python/price_processing.py
@@ -1,26 +1,36 @@
-import test_prices
 import tokens
 
-import copy
 import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
 def get_prices():
-    get_url = f"https://web-api.tp.entsoe.eu/api?securityToken={tokens.entsoe_token}\
-        documentType=A44&in_Domain=10YFI-1--------U&out_Domain=10YFI-1--------U&periodStart=202301010000&periodEnd=202312310000"
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y%m%d%H%M')
+    print(formatted_time)
+    get_url = f"https://web-api.tp.entsoe.eu/api?securityToken={tokens.entsoe_token}&documentType=A44&in_Domain=10YFI-1--------U&out_Domain=10YFI-1--------U&periodStart=202307282200&periodEnd=202312310000"
     response_text = requests.get(get_url).text
+    print(response_text)
     return response_text
 
 def get_texts():
 
-    prices = copy.deepcopy(get_prices())
-    price_number = len(prices)
+    try:
+        root = ET.fromstring(get_prices())
+    except ET.ParseError as e:
+        print("XML Parsing Error:", e)
 
     displayable_text = ""
 
-    for i in prices:
-        tags = "#" * i
-        displayable_text += f"{i}snt    {tags}\n"
+    for day in root.findall('{*}TimeSeries'):
+        for point in day.find("{*}Period").findall('{*}Point'):
+            hour = int(point.find('{*}position').text)
+            price = float(point.find('{*}price.amount').text)
+            price_snt_kwh = price / 10
+            price_snt_kwh_formatted = format(price_snt_kwh, '.2f')
+            tags = "#" * int(price_snt_kwh)
+            displayable_text += f"{hour}: {price_snt_kwh_formatted}snt    {tags}\n"
 
     return displayable_text
 
-print(get_prices())
+print(get_texts())
