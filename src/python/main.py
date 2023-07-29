f0a20a8232cc2e337723daaaf1551ccd5cde9274
apodl <a.podlozny@gmail.com>
2023-07-29 20:08:53 +0300

    Added help-message

--- src/python/main.py
+++ src/python/main.py
@@ -10,18 +10,26 @@ logging.basicConfig(
     level = logging.INFO
 )
 
+with open('help_message.txt', 'r') as file:
+    help_message = file.read()
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id = update.effective_chat.id, text = "Sähkön hinnan kertova botti. Lähetä komento /hinta saadaksesi vastauksen.")
+    await context.bot.send_message(chat_id = update.effective_chat.id, text = "Pörssisähkön tulevan tuntihinnan kertova botti. Lähetä komento /hinta saadaksesi vastauksen tai /help pyytääksesti lisää tietoa.")
 
 async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id = update.effective_chat.id, text = price_processing.construct_texts())
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id = update.effective_chat.id, text = help_message)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(tokens.telegram_token).build()
     
     start_handler = CommandHandler('start', start)
     price_handler = CommandHandler('hinta', price)
+    help_handler = CommandHandler('help', help)
     application.add_handler(start_handler)
     application.add_handler(price_handler)
+    application.add_handler(help_handler)
     
     application.run_polling()
