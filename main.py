3ba13249c0bd12d3d5052ee5c5144592d7b8cf9b
apodl <a.podlozny@gmail.com>
2024-01-11 15:51:53 +0200

    Improved comments

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import schedule
 import asyncio
 
+#set logging
 logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level = logging.CRITICAL
 )
 
+#read bot start and help messages from files
 with open('messages/start_message.txt', 'r', encoding='utf-8') as file:
     start_message = file.read().rstrip()
 
 with open('messages/help_message.txt', 'r', encoding='utf-8') as file:
     help_message = file.read().rstrip()
 
+#read api-token for telegram bot
 with open('telegram_token.txt', 'r') as file:
     telegram_token = file.read().rstrip()
 
 
+#functions for sending messages, called by handlers later
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id = update.effective_chat.id, text = start_message)
 
@@ -30,17 +34,17 @@ async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id = update.effective_chat.id, text = help_message)
 
-
+#infinite loop for running scheduled jobs 
 async def schedule_loop():
     while True:
         schedule.run_pending()
-        print("waiting")
         await asyncio.sleep(1 * 60 * 15)
 
-
+#main block
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_token).build()
     
+    #handlers for messages
     start_handler = CommandHandler('start', start)
     price_handler = CommandHandler('hinta', price)
     help_handler = CommandHandler('help', help)
@@ -48,11 +52,11 @@ if __name__ == '__main__':
     application.add_handler(price_handler)
     application.add_handler(help_handler)
     
-
+    #get initial prices
     price_processing.get_prices()
 
+    #schedule price updating at 16:00 every day
     schedule.every().day.at("16:00").do(price_processing.get_prices)
-
     asyncio.ensure_future(schedule_loop())
     asyncio.get_event_loop().run_forever
 
--- price_processing.py
+++ price_processing.py
@@ -2,8 +2,10 @@ import requests
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta, time, timezone
 
+#initial value
 prices = ""
 
+#function to get prices from API
 def get_prices():
     current_UTC = datetime.now(timezone.utc).replace(tzinfo=None)
     previous_UTC = datetime.combine(current_UTC, time.min) - timedelta(days=1)
@@ -11,22 +13,25 @@ def get_prices():
     period_end = datetime.combine(current_UTC, time.min) + timedelta(days=3)
     period_end_formatted = period_end.strftime("%Y%m%d%H%M")
 
+    #read api-token for ENTSOE
     with open('entsoe_token.txt', 'r') as file:
         entsoe_token = file.read().rstrip()
 
+    #url for api-request
     get_url = f"https://web-api.tp.entsoe.eu/api?securityToken={entsoe_token}&documentType=A44&in_Domain=10YFI-1--------U&out_Domain=10YFI-1--------U&periodStart={previous_UTC_formatted}&periodEnd={period_end_formatted}"
+    
     try:
-        print(f"Request sent at {datetime.now()}")
+        print(f"Request sent at {datetime.now()}") #log
         response_text = requests.get(get_url).text
     except requests.RequestException as e:
-        print(f"An error occurred during the request: {e}")
+        print(f"An error occurred during the request: {e}") #log
         return None
     
-    print(f"Answer received at at {datetime.now()}")
+    print(f"Answer received at at {datetime.now()}") #log
 
     global prices
-    prices_updated_flag = (response_text == prices)
-    prices = response_text
+    prices_updated_flag = (response_text == prices) #set flag if new prices (currently not used)
+    prices = response_text #update the prices with the response
 
 
     return prices_updated_flag
@@ -40,35 +45,42 @@ def construct_texts():
     timezone_difference_h = (current_HEL - current_UTC).seconds / 3600
     current_HEL_hour = current_HEL.strftime("%H")
 
+    #parse to XML
     try:
         root = ET.fromstring(prices)
     except ET.ParseError as e:
         print("XML Parsing Error:", e)
 
+    #initial value
     displayable_text = ""
 
+    #hour at which the API-data starts each day
     start_hour = 23
+    #collector
     prices_printed = 0
+    #maximum number of prices in the response
     prices_to_print = 20
 
+    #construct the reponse-message by parsing XML
     for day in root.findall('{*}TimeSeries'):
-        end_date = day.find("{*}Period").find("{*}timeInterval").find("{*}end").text[:10]
+        end_date = day.find("{*}Period").find("{*}timeInterval").find("{*}end").text[:10] #find date of last day
         now_date = current_HEL.strftime("%Y-%m-%d")[:10]
-        if now_date <= end_date:
-            for point in day.find("{*}Period").findall('{*}Point'):
-                if prices_printed >= prices_to_print:
+        if now_date <= end_date: #if future prices exist
+            for point in day.find("{*}Period").findall('{*}Point'): #find all points (= hourly prices)
+                if prices_printed >= prices_to_print: #if enough prices
                     break
-                index_of_point = int(point.find('{*}position').text)
-                hour_abs = (start_hour + index_of_point - 1)
-                HEL_hour_abs = int(hour_abs + timezone_difference_h)
-                HEL_hour_real = HEL_hour_abs % 24
-                if now_date < end_date or HEL_hour_real >= int(current_HEL_hour) or HEL_hour_abs == 48: #a bit of a band-aid :D
-                    price = float(point.find('{*}price.amount').text)
-                    price_snt_kwh = price / 10
-                    price_snt_kwh_formatted = format(price_snt_kwh, '.2f')
-                    tags = "#" * round(price_snt_kwh * 2)
-                    hours_to_price = (prices_printed)
-                    displayable_text += f"{HEL_hour_real:0{2}d} [{hours_to_price:0{2}d}]:   {price_snt_kwh_formatted}snt    {tags}\n"
-                    prices_printed += 1
+                index_of_point = int(point.find('{*}position').text) #the points are only indexed starting from day 1 23:00 UTC and go until day 2 22:00 UTC, points do not contain time info other than the start
+                hour_abs = (start_hour + index_of_point - 1) #get hours from start by 23 (start of data) + index - 1
+                HEL_hour_abs = int(hour_abs + timezone_difference_h) #convert to UTC+2
+                HEL_hour_real = HEL_hour_abs % 24 #convert to real hours (eg. 24 -> 00, 25 -> 01...)
+                if now_date < end_date or HEL_hour_real >= int(current_HEL_hour) or HEL_hour_abs == 48: #if hour in the future -> add to message
+                    price = float(point.find('{*}price.amount').text) #get price from point
+                    price_snt_kwh = price / 10 #to snt/kwh
+                    price_snt_kwh_formatted = format(price_snt_kwh, '.2f') #format
+                    tags = "#" * round(price_snt_kwh * 2) #visualize with "#...#"
+                    hours_to_point = prices_printed #hours until current point
+                    displayable_text += f"{HEL_hour_real:0{2}d} [{hours_to_point:0{2}d}]:   {price_snt_kwh_formatted}snt    {tags}\n" #construct final text
+                    prices_printed += 1 #increment
 
+    #return to caller
     return displayable_text
