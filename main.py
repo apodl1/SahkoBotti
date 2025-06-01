56681fbabd0ca4a6bba1f165b4849807540a9d6a
apodl <a.podlozny@gmail.com>
2025-06-01 22:49:35 +0300

    fix(main): fix comment typos

--- main.py
+++ main.py
@@ -70,19 +70,19 @@ async def fetch_with_retry_job(context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
   setup_logger()
 
-  elec_prices = ElecPrices() #prices fetched and sotred on construction
+  elec_prices = ElecPrices() #prices fetched and stored on construction
   application = ApplicationBuilder().token(telegram_token).build()
 
   #handlers for messages
   start_handler = CommandHandler('start', start)
-  price_handler = CommandHandler('hinta', partial(price, prices_object=elec_prices)) #pass prices-object to callback via partial
+  price_handler = CommandHandler('hinta', partial(price, prices_object=elec_prices)) #pass prices-object to callback via a partial
   help_handler = CommandHandler('help', help)
   application.add_handler(start_handler)
   application.add_handler(price_handler)
   application.add_handler(help_handler)
 
-  assert application.job_queue #this exists if requiremets installed
-  # Schedule fetching new prices, the calLed function recurses and runs again in 1h if new prices not available (or fetch fails)
+  assert application.job_queue #this exists if requirements.txt installed
+  # Schedule fetching new prices, the called function recurses and runs again in 1h if new prices not yet available or the fetch fails
   application.job_queue.run_daily(
       fetch_with_retry_job,
       time=time(hour=PRICE_UPDATE_HOUR, minute=0),
